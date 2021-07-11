aaac2a028832fa56f1df23f8a89b46f77c96ee9f
junko0126 <hikari2011h.j@gmail.com>
2021-07-11 21:11:19 +0900

    クリックイベントの修正

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <input type="text" v-model="text" />
-    <button @click="$router.push({ text })" class="user_btn">住所自動入力</button>
+    <button @click="userClick" >住所自動入力</button>
     <p>Address:{{ text }}</p>
   </div>
 </template>
@@ -14,13 +14,18 @@ export default {
       text: ""
     };
   },
-  async created() {
-    const item = await axios.get(
+  methods: {
+    userClick() {
+      this.text
+    },
+    async mounted
+    const item = axios.get(
       `https://apis.postcode-jp.com/api/v4/postcodes/1000001?apikey=scXXQD1i29VdtnFS1QOuruB2oG6GIyNseCmaqE`
     );console.log(item);
     
     const postCode =item.data;
     this.postcode = postCode.postcode;
+    this.text = postCode.AllAddress;
   }
 };
 </script>
